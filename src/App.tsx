f79a1cb15135e5a3166250c074c011bfde91b128
LLmoskk <1398145450@qq.com>
2023-06-30 22:27:41 +0800

    fix: svg path error

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { defineComponent } from "vue";
-import HelloWorld from './components/HelloWorld'
-import './App.css'
+import HelloWorld from "./components/HelloWorld";
+import "./App.css";
+import ViteLogo from "../public/vite.svg";
+import VueLogo from "../public/vue.svg";
+
 export default defineComponent({
   setup() {
     return () => (
       <>
         <div>
           <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
+            <img src={ViteLogo} class="logo" alt="Vite logo" />
           </a>
           <a href="https://vuejs.org/" target="_blank">
-            <img src="/vue.svg" class="logo vue" alt="Vue logo" />
+            <img src={VueLogo} class="logo vue" alt="Vue logo" />
           </a>
         </div>
         <HelloWorld msg="Vite + Vue + TSX" />
